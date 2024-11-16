819cec1b6eb564e71a5b2ebc3a334e6bf246689e
Richard Carvalho <richardcarvalhooo@gmail.com>
2024-11-15 23:17:29 -0300

    style: improving home visual

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { ReactComponent as Logout } from "../../images/log-out.svg";
 import "./index.css";
 
 function Home() {
@@ -10,11 +11,56 @@ function Home() {
   };
 
   return (
-    <div>
-      <h1>Home</h1>
-      <button onClick={() => navigate("/characters")}>See characters</button>
-      <button onClick={() => navigate("/events")}>See events</button>
-      <button onClick={handleLogout}>Logout</button>
+    <div className="home-container">
+      <div className="top-bar">
+        <h1>Home</h1>
+        <button onClick={handleLogout} className="logout-button">
+          <Logout className="logout" />
+        </button>
+      </div>
+      <div className="home-content">
+        <div className="content-wrapper">
+          <div className="content">
+            <div className="content-text">
+              <h3 className="content-title">Characters</h3>
+              <span className="content-description">
+                Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+              </span>
+            </div>
+            <div className="button-container">
+              <button
+                onClick={() => navigate("/characters")}
+                className="table-button"
+              >
+                See characters
+              </button>
+            </div>
+          </div>
+          <div className="content">
+            <div className="content-text">
+              <h3 className="content-title">Events</h3>
+              <span className="content-description">
+                Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+              </span>
+            </div>
+            <div className="button-container">
+              <button
+                onClick={() => navigate("/events")}
+                className="table-button"
+              >
+                See events
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
